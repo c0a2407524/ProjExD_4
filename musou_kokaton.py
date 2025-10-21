e7dd4c09bbbb51b0fb744c9082482cc510db6b74
C0A24075 <c0a2407524@edu.teu.ac.jp>
2025-10-21 15:25:21 +0900

    追加機能2＿実装完了

--- musou_kokaton.py
+++ musou_kokaton.py
@@ -241,6 +241,40 @@ class Score:
         self.image = self.font.render(f"Score: {self.value}", 0, self.color)
         screen.blit(self.image, self.rect)
 
+class Gravity(pg.sprite.Sprite):
+    """
+    画面全体を覆う重力場の発生するクラス
+    """
+
+    def __init__(self, life):
+        """
+        引数
+        life:重力場を発生させる時間
+        """
+        super().__init__()
+        self.life = life
+        self.image = pg.Surface((WIDTH, HEIGHT), pg.SRCALPHA).convert_alpha()
+        pg.draw.rect(self.image, (0, 0, 0, 150), (0, 0, WIDTH, HEIGHT))
+        self.rect = self.image.get_rect()
+
+    def update(self, bombs, emys, exps, score):
+        self.life -= 1
+        if self.life < 0:
+            self.kill()
+
+        hit_bombs = pg.sprite.spritecollide(self, bombs, True)
+        for bomb in hit_bombs:
+            exps.add(Explosion(bomb, 50))
+            score.value += 1
+
+        hit_enemies = pg.sprite.spritecollide(self, emys, True)
+        for emy in hit_enemies:
+            exps.add(Explosion(emy, 100))
+            score.value += 1
+
+
+        
+
 
 def main():
     pg.display.set_caption("真！こうかとん無双")
@@ -253,6 +287,8 @@ def main():
     beams = pg.sprite.Group()
     exps = pg.sprite.Group()
     emys = pg.sprite.Group()
+    gravities = pg.sprite.Group()
+    
 
     tmr = 0
     clock = pg.time.Clock()
@@ -288,6 +324,11 @@ def main():
             pg.display.update()
             time.sleep(2)
             return
+        
+        if key_lst[pg.K_RETURN] and score.value > 200:
+            score.value -= 200
+            gravities.add(Gravity(400))
+           
 
         bird.update(key_lst, screen)
         beams.update()
@@ -295,10 +336,13 @@ def main():
         emys.update()
         emys.draw(screen)
         bombs.update()
+
         bombs.draw(screen)
         exps.update()
         exps.draw(screen)
         score.update(screen)
+        gravities.draw(screen)
+        gravities.update(bombs, emys, exps, score)
         pg.display.update()
         tmr += 1
         clock.tick(50)
